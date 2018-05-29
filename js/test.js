373f0c8a261f5bdfc5cc8946c91af34906ea181d
superrabbitrun007` <244398764@qq.com>
2018-05-29 20:10:44 +0800

    增加function show()中判断条件，避免多次点击出现错误

--- js/test.js
+++ js/test.js
@@ -41,15 +41,19 @@ shuffle(array);
 function showCard(event) {
   //将所点击的卡片的class值赋值给tags
   let tags = event.target.classList.value;
+  //增加判断，避免多次点击同一卡片时，多次更新tags的值（即只在第一次点击时添加show）
+  if (tags === 'card') {
+    //给tags添加show类
+    tags = tags + ' show';
+    //连接到html中，更新html中该卡片的li-class值
+    event.target.classList.value = tags;
+    // //test代码
+    // console.log(event.target.classList.value);
+    // console.log(tags);
+    //测试更新class值是否成功（为什么此时输出时open类已经添加成功？）
+    console.log(event.target);
+  }
 
-  //给tags添加show类
-  tags = tags + ' show';
-  //连接到html中，更新html中该卡片的li-class值
-  event.target.classList.value = tags;
-  // //test代码
-  // console.log(event.target.classList.value);
-  // console.log(tags);
-  console.log(event.target);
 
 }
 
@@ -58,11 +62,13 @@ function openArray(event) {
     let tags = event.target.classList.value;
     tags = 'card open show';
     event.target.classList.value = tags;
+    //增加计数，便于向数组中存入更新后的event.target
     count = count +1;
     console.log(count);
 
-
+    //设置数组的长度和页面卡片数组的长度相同（此时arrayOpen数组为空数组，但长度已经设置）
     arrayOpen.length = array.length;
+    //由于点击之后count的最小值为1，数组index需要从0开始，所以此处count-1
     arrayOpen[count-1] = event.target;
     console.log(arrayOpen);
 
@@ -97,6 +103,7 @@ function ll(event) {
   openArray(event);
   console.log(count);
   // console.log(arrayOpen[2]);
+  matchCard();
 
 
   // openArray();
